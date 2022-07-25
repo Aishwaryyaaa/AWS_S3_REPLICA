3a8f925d6e117a9f86a0348f73c4f588af26d8ed
aishwaryyaaa <aishwarya0219@gmail.com>
2022-07-25 19:40:55 +0530

    my

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+  type    = string
+  default = "ami-0960ab670c8bb45f3"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+# variable "public_vpc_cidr" {
+#   type = list(string)
+# }
+# variable "avail_zone" {
+#   type = list(string)
+# }
+# variable "private_vpc_cidr" {
+#   type = list(string)
+# }
